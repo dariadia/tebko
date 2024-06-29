50654bf48a5ae01167be3d5f33f3bd6e9e8b0a33
Daria Diachkova <daria.diachkova@mail.ru>
2024-06-29 19:44:57 +0300

    fix board update

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import { ref, reactive, onBeforeMount, computed } from 'vue'
+import { ref, onBeforeMount, computed } from 'vue'
 import nestedDraggable from 'vuedraggable'
 
 const CORNERS = [0, 1, 5, 6]
-let board = reactive([[1]])
+let board = ref([[1]])
 
 onBeforeMount(() => {
-  board = Array.from({ length: 7 }, (_, row) =>
+  board.value = Array.from({ length: 7 }, (_, row) =>
     Array.from({ length: 7 }, (_, col) => {
       if (row === 3 && col === 3) return 0
       if (CORNERS.includes(row) && CORNERS.includes(col)) return -1
@@ -15,7 +15,7 @@ onBeforeMount(() => {
   )
 })
 const playing = computed(() => {
-  return board.some((row) => row.some((piece) => piece === 1))
+  return board.value.some((row) => row.some((piece) => piece === 1))
 })
 const drag = ref(false)
 </script>
@@ -32,16 +32,16 @@ const drag = ref(false)
   <main style="padding-top: 42px; max-width: fit-content; margin: auto; display: block">
     <nested-draggable v-model="board" @start="drag = true" @end="drag = false" item-key="col">
       <template #item="{ element, row }">
-        <v-row :key="row" style="display: block">
-          <v-col
+        <div :key="row">
+          <div
             v-for="(item, col) in element"
             :key="col"
             style="padding: 20px 26px; border: 1px solid grey; display: inline-block"
           >
             <span v-if="item !== -1">{{ item }}</span>
             <span v-else style="opacity: 0">1</span>
-          </v-col>
-        </v-row>
+          </div>
+        </div>
       </template>
     </nested-draggable>
   </main>
