403cd0cbb664b84937e7718581587ef94403cae0
Daria Diachkova <daria.diachkova@mail.ru>
2024-06-29 19:23:49 +0300

    set up board

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import { ref, reactive, onMounted } from 'vue'
-const TOTAL = 32
+import { ref, reactive, onBeforeMount, computed } from 'vue'
+import nestedDraggable from 'vuedraggable'
+
 const CORNERS = [0, 1, 5, 6]
-let board = reactive([[0]])
-onMounted(() => {
+let board = reactive([[1]])
+
+onBeforeMount(() => {
   board = Array.from({ length: 7 }, (_, row) =>
     Array.from({ length: 7 }, (_, col) => {
       if (row === 3 && col === 3) return 0
@@ -12,10 +14,9 @@ onMounted(() => {
     })
   )
 })
-
-// const publishedBooksMessage = computed(() => {
-//   return author.books.length > 0 ? 'Yes' : 'No'
-// })
+const playing = computed(() => {
+  return board.some((row) => row.some((piece) => piece === 1))
+})
 const drag = ref(false)
 </script>
 
@@ -26,19 +27,17 @@ const drag = ref(false)
       >Головоломка символизирует стадо оленей в тундре. Задача игрока - собрать стадо вместе, согнав
       их в центр.</small
     >
+    <h4 v-if="!playing">Вы выиграли</h4>
   </header>
-  <main>
-    {{ board }}
-    <draggable
-      v-model="board"
-      group="people"
-      @start="drag = true"
-      @end="drag = false"
-      item-key="id"
-    >
+  <main style="padding-top: 100px; max-width: 900px; margin: auto; display: block">
+    <nested-draggable v-model="board" @start="drag = true" @end="drag = false" item-key="id">
       <template #item="{ element }">
-        <div>{{ element.name }}</div>
+        <v-row style="display: block">
+          <v-col v-for="item in element" style="padding: 36px; border: 1px solid grey">{{
+            item
+          }}</v-col>
+        </v-row>
       </template>
-    </draggable>
+    </nested-draggable>
   </main>
 </template>
