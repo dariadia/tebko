29ee19d30d3859e88b47672cc02781ce1c7b895f
Daria Diachkova <daria.diachkova@mail.ru>
2024-06-29 18:28:58 +0300

    add board

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,44 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, reactive, onMounted } from 'vue'
+const TOTAL = 32
+const CORNERS = [0, 1, 5, 6]
+let board = reactive([[0]])
+onMounted(() => {
+  board = Array.from({ length: 7 }, (_, row) =>
+    Array.from({ length: 7 }, (_, col) => {
+      if (row === 3 && col === 3) return 0
+      if (CORNERS.includes(row) && CORNERS.includes(col)) return -1
+      else return 1
+    })
+  )
+})
+
+// const publishedBooksMessage = computed(() => {
+//   return author.books.length > 0 ? 'Yes' : 'No'
+// })
+const drag = ref(false)
+</script>
 
 <template>
-  <header></header>
-  <main></main>
+  <header style="text-align: center">
+    <h1>Ненецкая головоломка Тебко</h1>
+    <small
+      >Головоломка символизирует стадо оленей в тундре. Задача игрока - собрать стадо вместе, согнав
+      их в центр.</small
+    >
+  </header>
+  <main>
+    {{ board }}
+    <draggable
+      v-model="board"
+      group="people"
+      @start="drag = true"
+      @end="drag = false"
+      item-key="id"
+    >
+      <template #item="{ element }">
+        <div>{{ element.name }}</div>
+      </template>
+    </draggable>
+  </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
